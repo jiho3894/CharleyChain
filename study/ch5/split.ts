5391aee6ae7deae81cde7efeacea5e9601c94594
김지호 <crsn1111@gmail.com>
2022-07-10 14:04:32 +0900

    create interface

---
+++ study/ch5/split.ts
@@ -0,0 +1,36 @@
+const MAX_ARRAY_LENGTH = 4294967295;
+
+/**
+ * Splits `string` by `separator`.
+ *
+ * **Note:** This method is based on
+ * [`String#split`](https://mdn.io/String/split).
+ *
+ * @since 4.0.0
+ * @category String
+ * @param {string} [string=''] The string to split.
+ * @param {RegExp|string} separator The separator pattern to split by.
+ * @param {number} [limit] The length to truncate results to.
+ * @returns {Array} Returns the string segments.
+ * @example
+ *
+ * split('a-b-c', '-', 2)
+ * // => ['a', 'b']
+ */
+
+interface type {
+  string: string;
+  separator: string;
+  limit: number;
+}
+
+function split({ string, separator, limit }: type) {
+  limit = limit === undefined ? MAX_ARRAY_LENGTH : limit >>> 0;
+  if (!limit) {
+    return [];
+  }
+  if (string && (typeof separator === "string" || separator != null))
+    return string.split(separator, limit);
+}
+
+export default split;
